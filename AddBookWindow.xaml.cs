0bd7cfb6cb8b872b6cab3a29745dc95fda4253b9
Justin Herr <herrjustin0@gmail.com>
2024-11-01 11:25:19 -0500

    Add project files.

---
+++ AddBookWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static book_tracker.MainWindow;
+
+namespace book_tracker
+{
+    public partial class AddBookWindow : Window
+    {
+        string connectionString = @"Server=server_name;Port=port_number;User Id=postgres;Password=server_pw;Database=name_of_database";
+        NpgsqlCommand command = new NpgsqlCommand();
+
+        
+
+        public AddBookWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            using (NpgsqlConnection npgCon = new NpgsqlConnection(connectionString))
+            {
+                npgCon.Open();
+                NpgsqlCommand checkBookName = new NpgsqlCommand("select count (*) from light_novel where title ilike '%' || @bookName || '%'", npgCon);
+                checkBookName.Parameters.AddWithValue("@bookName", TypeBookName.Text);
+                int BookExist = (int)(long)checkBookName.ExecuteScalar();
+
+                // if book exist, the count in the Db will be 1 which is greater than 0 meaning it exist
+                if (BookExist > 0)
+                {
+                    FindDupeTest.Text = "book is in Db or empty value";
+
+                } 
+                else
+                {
+                    // clear the textblock when the add button is click to check if book exist in DB
+                    FindDupeTest.Text = string.Empty;
+
+                    // add title and book image for now to Db
+                    NpgsqlCommand addBook = new NpgsqlCommand("insert into light_novel (title, book_image) values (@title, @bookImage)", npgCon);
+                    addBook.Parameters.AddWithValue("@title", TypeBookName.Text);
+                    addBook.Parameters.AddWithValue("@bookImage", FileName.Text);
+
+                    //ExecuteNonQuery is needed to perform the insert into on Postgres
+                    addBook.ExecuteNonQuery();
+
+                    Books books = new Books();
+                    
+                    // clear the text from textbox and file 
+                    TypeBookName.Clear();
+                    FileName.Text = string.Empty;
+
+                    FindDupeTest.Text = "Sucessfully Added to Db";
+
+             
+                     // this window close when add is successfully done
+                    Close();
+                }
+                npgCon.Close();
+                
+               
+
+            }
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".jpg";
+            dlg.Filter = "JPG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|JPEG Files (*.jpeg)|*.jpeg|GIF Files (*.gif)|*.gif";
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                FileName.Text = filename;
+            }
+        }
+
+
+        // messing around with button disappear on click
+       /* private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            //Hides Current Button
+            button1.Visibility = Visibility.Collapsed;
+
+            //Checks and Shows Button 2
+            if (button2.Visibility == Visibility.Collapsed)
+            {
+                button2.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            //Hides Current Button
+            button2.Visibility = Visibility.Collapsed;
+
+            //Checks and Shows Button 1
+            if (button1.Visibility == Visibility.Collapsed)
+            {
+                button1.Visibility = Visibility.Visible;
+            }
+        }*/
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System.Data;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Npgsql;
+using Npgsql.Replication;
+using Microsoft.VisualBasic;
+using System.Numerics;
+using System.Collections.Specialized;
+using System.Security.AccessControl;
+using System.Collections.ObjectModel;
+
+// will want to remove this if do not use it
+namespace System.Windows.Controls
+{
+    public static class MyExt
+    {
+        // Adding function that is on Forms to WPF
+        public static void PerformClick(this Button btn)
+        {
+            btn.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+        }
+    }
+}
+
+namespace book_tracker
+{
+    public partial class MainWindow : Window
+    {
+        public class Books
+        {
+            public string list_novelid { get; set; }
+            public string list_bookname { get; set; }
+            public string list_book_buy { get; set; }
+            public string list_book_total { get; set; }
+            public string list_book_image { get; set; }
+
+        }
+
+        string connectionString = @"Server=server_name;Port=port_number;User Id=postgres;Password=server_pw;Database=name_of_database";
+        NpgsqlCommand command = new NpgsqlCommand();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            TotalCollected();
+            listDisplay();
+            
+        }
+
+       // public ObservableCollection<Item> Items { get; } = new ObservableCollection<Item>();
+
+        private void listDisplay()
+        {
+            using (NpgsqlConnection npgCon = new NpgsqlConnection(connectionString))
+            {
+                npgCon.Open();
+                NpgsqlCommand npgQuery = new NpgsqlCommand("Select * from light_novel", npgCon);
+                npgQuery.Connection = npgCon;
+                NpgsqlDataAdapter npgAdapter = new NpgsqlDataAdapter(npgQuery);
+
+                DataTable bookDataTable = new DataTable();
+                npgAdapter.Fill(bookDataTable);
+
+                // total count of the list from database
+                int lstCount = bookDataTable.Rows.Count;
+                
+
+                int curCount = lstCount;
+
+                int i = 0;
+
+                List<Books> items = new List<Books>();
+                while (lstCount > i)
+                {
+                    items.Add(new Books()
+                    {
+                        list_novelid = bookDataTable.Rows[i]["novel_id"].ToString(),
+                        list_bookname = bookDataTable.Rows[i]["title"].ToString(),
+                        list_book_buy = bookDataTable.Rows[i]["vol_bought"].ToString(),
+                        list_book_total = bookDataTable.Rows[i]["total_vol"].ToString(),
+                        list_book_image = bookDataTable.Rows[i]["book_image"].ToString()
+                    });
+
+                   
+                    displayTest.ItemsSource = items;
+                    i++;
+
+                    // filter through book names from list and refresh the list
+                    CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(displayTest.ItemsSource);
+                    view.Filter = UserBookFilter;
+
+                }
+
+                npgCon.Close();
+            }
+        }
+
+        private void TotalCollected()
+        {
+            using (NpgsqlConnection npgCon = new NpgsqlConnection(connectionString))
+            {
+                npgCon.Open();
+                command.Connection = npgCon;
+                command.CommandText = "SELECT SUM(vol_bought) FROM light_novel"; 
+                TotalBooks.Text = command.ExecuteScalar().ToString(); //green underline means warning
+                npgCon.Close();
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private bool UserBookFilter(object item)
+        {
+            if (string.IsNullOrEmpty(SearchBox.Text))
+                return true;
+            else
+                return ((item as Books).list_bookname.IndexOf(SearchBox.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+
+        private void BookFilter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(displayTest.ItemsSource).Refresh();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddBookWindow addBookWindow = new AddBookWindow();
+            addBookWindow.Show();
+        }
+
+        private void RefreshBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow newWindow = new MainWindow();
+            Application.Current.MainWindow = newWindow;
+            newWindow.Show();
+            Close();
+        }
+    }
+}
